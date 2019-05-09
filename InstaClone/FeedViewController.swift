5e0b8a67f1cd8c3563527a2eba0af4afdcd07978
Kunal <kunal.ajay@gmail.com>
2019-05-09 14:00:48 -0700

    UI Changes and Bonus feature

--- InstaClone/FeedViewController.swift
+++ InstaClone/FeedViewController.swift
@@ -13,6 +13,8 @@ import AlamofireImage
 class FeedViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     var posts = [PFObject]()
+    var refreshControl: UIRefreshControl!
+    var numberOfPosts : Int!
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -20,13 +22,45 @@ class FeedViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        numberOfPosts = 20
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = numberOfPosts
+        query.findObjectsInBackground { (feed, error) in
+            if(feed != nil){
+                self.posts = feed!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    @objc func onRefresh() {
+        numberOfPosts = 3
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
+        query.findObjectsInBackground { (feed, error) in
+            if(feed != nil){
+                self.posts = feed!
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+            }
+        }
+    }
+    
+    func loadMorePosts(){
+        numberOfPosts = numberOfPosts + 4
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
         query.findObjectsInBackground { (feed, error) in
             if(feed != nil){
                 self.posts = feed!
@@ -38,6 +72,13 @@ class FeedViewController: UIViewController,UITableViewDelegate, UITableViewDataS
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
     }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if(indexPath.row + 1 == posts.count && posts.count == numberOfPosts){
+            loadMorePosts()
+        }
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
